854e309f1b610060cf5c42278f82369893845a0a
Kedar59 <kedarshidhaye2003@gmail.com>
2023-02-28 12:04:00 +0530

    php my admin added

--- trial.php
+++ trial.php
@@ -1,6 +1,6 @@
 <?php
 $timezone=date_default_timezone_set("Indian/Maldives");
-$con=mysqli_connect("localhost","root","MosH@1211","employeedb");
+$con=mysqli_connect("sq l12.freemysqlhosting.net","sql12600177","6cBLLGdzBK","sql12600177");
 
 if(mysqli_connect_errno()){
     echo "failed to connect: " . mysqli_connect_errno();
