31a8156afd041a5a2f2a211b0d4507e1796ce993
Kedar59 <kedarshidhaye2003@gmail.com>
2023-03-31 20:33:30 +0530

    admin login check

--- admin_login.php
+++ admin_login.php
@@ -12,7 +12,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   $num = mysqli_num_rows($result);
   $res = mysqli_fetch_array($result);
   
-  if($num==1 && (password_verify($password,$res['password'])))
+  if($num==1 && $password==$res['password'])
   {
     $login=true;
     session_start();
@@ -34,7 +34,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
   <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Login or register</title>
+    <title>admin Login</title>
     <script src="https://kit.fontawesome.com/f91c2540fb.js" crossorigin="anonymous"></script>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
     <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
