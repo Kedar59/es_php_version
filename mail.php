6e5bea91b7dc0644630a0a49a6233ac87742ff4c
NarutoUchiha39 <anirudhata123@gmail.com>
2023-04-02 08:49:36 +0530

    Removed password

--- mail.php
+++ mail.php
@@ -15,7 +15,7 @@ function SendMail($subject,$body)
         $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
         $mail->Username   = 'anirudhata123@gmail.com';                     //SMTP username
-        $mail->Password   = 'ddkharwmzkjvexqs';                               //SMTP password
+        $mail->Password   = '';                               //SMTP password
         $mail->SMTPSecure = 'tls';            //Enable implicit TLS encryption
         $mail->Port       = 587;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
