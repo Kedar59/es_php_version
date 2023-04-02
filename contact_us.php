217a058b559e820b6178b3c8929632d321baef31
Kedar59 <kedarshidhaye2003@gmail.com>
2023-04-02 20:44:36 +0530

    redirect correted in cont us

--- contact_us.php
+++ contact_us.php
@@ -52,7 +52,7 @@
       <div class="right-side">
         <div class="topic-text">Send us a message</div>
         <p>If you have any work from me or any types of quries related to my tutorial, you can send me message from here. It's my pleasure to help you.</p>
-      <form action="send.php" method="post">
+        <form method="post" class="form" action="contact_us.php">
         <div class="input-box">
           <input type="text" name="name" placeholder="Enter your name">
         </div>
