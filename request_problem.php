5d5d41b72871c5762bdfe20f8a80e48adf9a7883
Kedar59 <kedarshidhaye2003@gmail.com>
2023-04-25 00:08:40 +0530

    made request problems and submit solution page mailable

--- request_problem.php
+++ request_problem.php
@@ -27,7 +27,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Solutions</title>
+    <title>Request Problems</title>
    
     <link rel="stylesheet" href="styles/req_prob.css">
     
--- submit_solns.php
+++ submit_solns.php
@@ -1,17 +1,63 @@
+<?php
+    include("header.php");
+    include("mail.php");
+
+    if($_SERVER["REQUEST_METHOD"]=="POST")
+    {
+        $Name = $_POST["name"];
+        $Email = $_POST['email'];
+        $ProblemName = $_POST['ProblemName'];
+        $Link = $_POST['Link'];
+
+        $body = "Name : $Name"."<br><br>".
+                "Email : $Email"."<br><br>".
+                "Problem Name : $ProblemName"."<br><br>".
+                "Link : $Link";
+
+        SendMail("Submit Solution",$body);
+
+    }
+   
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
   <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Solutions</title>
-    <?php
-    include("header.php");
-    ?>
-    <link rel="stylesheet" href="styles/solutions.css">
+    <title>Request Problems</title>
+   
+    <link rel="stylesheet" href="styles/req_prob.css">
     
   </head>
   <body>
-    
+    <section class="container">
+      <h1 id="title">Submit Solution Form</h1>
+      <form method="post" class="form" action="submit_solns.php">
+        <div class="input-box">
+          <label>Full Name</label>
+          <input type="text" placeholder="Enter full name" name="name" required style="color:black" />
+        </div>
+
+        <div class="input-box">
+          <label>Email Address</label>
+          <input type="text" placeholder="Enter email address for contact" required name="email" style="color:black" />
+        </div>
+
+        <div class="column">
+          <div class="input-box">
+            <label>Problem name or link</label>
+            <input type="text" placeholder="Enter problem name" required name="ProblemName" style="color:black"/>
+          </div>
+          <div class="input-box">
+            <label>Solution link Pastebin Link</label>
+            <input type="text" placeholder="Enter problem link" required name="Link" style="color:black"/>
+          </div>
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
   </body>
 </html>
